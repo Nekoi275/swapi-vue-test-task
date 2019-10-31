e909976670180f4cb1c0ff864c878ed09e363e22
nekoi <nekoi275@gmail.com>
2019-11-01 01:36:52 +0200

    created range filtering

--- src/js/main.js
+++ src/js/main.js
@@ -53,7 +53,18 @@ var app = new Vue({
         },
         pilotsArr: [],
         shownFilters: 5,
-        filteredPilots: []
+        filteredPilots: [],
+        filters: {
+            pilots: [],
+            crewRange: {
+                min: 1,
+                max: 342953
+            },
+            psngsRange: {
+                min: 0,
+                max: 843342
+            }
+        }
     },
     watch: {
         filteredPilots: function () {
@@ -92,7 +103,7 @@ var app = new Vue({
             if (lastShips.length < 6) {
                 if (this.nextUrl) {
                     this.getData(this.nextUrl, this.processStarshipsData).then(function () {
-                        var nextShips = app.starshipsArr.slice(app.shownStarshipsCount, this.shownStarshipsCount + 6);
+                        var nextShips = app.starshipsArr.slice(app.shownStarshipsCount, app.shownStarshipsCount + 6);
                         app.showStarShips(nextShips);
                     });
                 } else {
@@ -140,48 +151,84 @@ var app = new Vue({
                 }
             }
 
-            this.shownStarships = this.shownStarships.filter(filterByPilots);
-        },
-
-        getPeople: function (nextUrl) {
-            if (nextUrl) {
-                this.getData(nextUrl, function (data) {
-                    app.pilotsArr = app.pilotsArr.concat(data.results.filter(function (pilot) {
-                        return pilot.starships.length > 0;
-                    }));
-                    app.getPeople(data.next);
-                });
+            var filterByCrew = function (ship) {
+                if (ship.crew === 'unknown') {
+                    return true;
+                } else {
+                    return (Number(ship.crew) >= app.filters.crewRange.min)
+                        && (Number(ship.crew) <= app.filters.crewRange.max);
+                }
             }
+
+            var filterByPassengers = function (ship) {
+                if (ship.passengers === 'unknown') {
+                    return true;
+                } else {
+                    return (Number(ship.passengers) >= app.filters.psngsRange.min)
+                        && (Number(ship.passengers) <= app.filters.psngsRange.max);
+                }
+            }
+
+this.shownStarships = this.shownStarships.filter(filterByPilots)
+    .filter(filterByCrew).filter(filterByPassengers);
         },
 
-        getData: function (url, dataFunc) {
-            return fetch(url)
-                .then(function (response) {
-                    return response.json();
-                })
-                .then(dataFunc);
-        }
+getPeople: function (nextUrl) {
+    if (nextUrl) {
+        this.getData(nextUrl, function (data) {
+            app.pilotsArr = app.pilotsArr.concat(data.results.filter(function (pilot) {
+                return pilot.starships.length > 0;
+            }));
+            app.getPeople(data.next);
+        });
+    }
+},
+
+getData: function (url, dataFunc) {
+    return fetch(url)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(dataFunc);
+}
     },
 
-    mounted: function () {
-        this.getData(this.nextUrl, this.processStarshipsData).then(function () {
-            app.showStarShips(app.starshipsArr.slice(0, 6));
-        });
+mounted: function () {
+    this.getData(this.nextUrl, this.processStarshipsData).then(function () {
+        app.showStarShips(app.starshipsArr.slice(0, 6));
+    });
 
-        this.getPeople('https://swapi.co/api/people/');
+    this.getPeople('https://swapi.co/api/people/');
 
-        var sliders = document.querySelectorAll('.range');
-        for (i = 0; i < sliders.length; i++) {
-            noUiSlider.create(sliders[i], {
-                start: [40, 60],
-                behaviour: 'drag',
-                connect: true,
-                range: {
-                    'min': 20,
-                    'max': 80
+    function initRange(selector, min, max, rangeName) {
+        noUiSlider.create(selector, {
+            start: [min, max],
+            connect: true,
+            range: {
+                'min': min,
+                '25%': max * 0.001,
+                '50%': max * 0.01,
+                '75%': max * 0.1,
+                'max': max
+            },
+            format: {
+                to: function (value) {
+                    return value.toFixed(0);
+                },
+                from: function (value) {
+                    return Number(value);
                 }
-            });
-        }
-    }
+            }
+        });
+        selector.noUiSlider.on('set', function (values) {
+            app.filters[rangeName].min = Number(values[0]);
+            app.filters[rangeName].max = Number(values[1]);
+            app.showStarShips();
+        });
+    };
+
+    initRange(document.querySelector('.crew-range'), 1, 342953, 'crewRange');
+    initRange(document.querySelector('.psngs-range'), 0, 843342, 'psngsRange');
+}
 });
 
